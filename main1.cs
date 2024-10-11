be57980dfcd4278a08da65c5f6766a78afb73fa8
Golgrax <70707230+Golgrax@users.noreply.github.com>
2024-10-11 18:54:59 +0800

    Create main1.cs

---
+++ main1.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the First number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the Second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the Third number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the Fourth number: ");
+        double num4 = Convert.ToDouble(Console.ReadLine());
+        double average = (num1 + num2 + num3 + num4) / 4;
+        Console.WriteLine($"The average of {num1}, {num2}, {num3}, {num4} is: {average}");
+    }
+}
